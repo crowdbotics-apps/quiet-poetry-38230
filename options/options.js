7e233b656487f3a989dd5df2f004a6522cd1e124
Crowdbotics <team@crowdbotics.com>
2023-01-09 12:52:19 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "quiet-poetry-38230",
+  url: "https://quiet-poetry-38230.botics.co",
+  api: "https://quiet-poetry-38230.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
